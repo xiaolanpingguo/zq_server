77c4acf30b64dae5677e4707a2bfd11633bee90a
xiaolanpingguo <xiaolanpingguo@163.com>
2018-11-29 10:18:33 +0800

    m

---
+++ src/server/test_server/test_session.cpp
@@ -0,0 +1,57 @@
+#include "test_session.h"
+#include "baselib/libloader/libmanager.h"
+#include "baselib/base_code/format.h"
+#include <iostream>
+#include "interface_header/base/IRedislModule.h"
+
+#if ZQ_PLATFORM == ZQ_PLATFORM_UNIX
+#include <libgo/coroutine.h>
+#endif
+
+using namespace std;
+
+namespace zq {
+
+
+
+void foo(TestSession* session)
+{
+	IRedisModule* redis_module = LibManager::get_instance().findModule<IRedisModule>();
+
+	std::string out;
+	static int i = 0;
+	redis_module->getClientBySuitConsistent()->GET(std::to_string(i++), out);
+
+	cout << "i: " << i << endl;
+}
+
+TestSession::TestSession(tcp::socket&& socket)
+	:InternalSocket(std::move(socket))
+{
+
+}
+TestSession::~TestSession()
+{
+
+}
+
+void TestSession::start()
+{
+	std::cout << "client has connect success, ip:" << getIp() <<" port:" << getPort() << std::endl;
+}
+
+void TestSession::onClose()
+{
+	std::cout << "client has disconnect, ip:" << getIp() << " port:" << getPort() << " id: " << getServerId() << std::endl;
+}
+
+void TestSession::readHandler()
+{
+#if ZQ_PLATFORM == ZQ_PLATFORM_UNIX
+	go std::bind(&foo, this);
+#endif
+}
+
+
+
+}
---
+++ src/server/test_server/test_session.h
@@ -0,0 +1,41 @@
+#pragma once 
+
+
+#include "baselib/network/internal_socket.hpp"
+#include "baselib/message/game_ss.pb.h"
+
+
+namespace SSMsg
+{
+	class SSPacket;
+}
+
+namespace zq {
+
+
+class TestSession : public InternalSocket
+{
+public:
+
+	TestSession(tcp::socket&& socket);
+	~TestSession();
+
+	void start() override;
+	void onClose() override;
+
+	void readHandler() override;
+
+	int getServerId() { return serverInfos_.server_id(); }
+	int getServerType() { return serverInfos_.server_type(); }
+	const SSMsg::ServerInfo& getServerInfo() { return serverInfos_; }
+
+public:
+	bool onS2SServerRegisterReq(const SSMsg::SSPacket& packet);
+	bool onS2SHeartBeat(const SSMsg::SSPacket& packet);
+
+private:
+
+	SSMsg::ServerInfo serverInfos_;
+};
+
+}
